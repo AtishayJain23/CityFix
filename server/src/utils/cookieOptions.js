e9f48ebf0fc2bd85131482cf303777ca8ea8e9fc
Atishay Jain <atishayjain2396@gmail.com>
2026-08-08 22:05:32 +0530

    testing

--- server/src/utils/cookieOptions.js
+++ server/src/utils/cookieOptions.js
@@ -1,6 +1,7 @@
 module.exports = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  //secure: process.env.NODE_ENV === "production",
+   secure: true,
   sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
